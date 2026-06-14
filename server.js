a9278f4cf6c32f38f068ec1b7604030cd19beb01
Phantom-Lemon <donggun0524@naver.com>
2026-06-14 19:03:23 +0900

    fetch를 통한 JSON보내기 받기 제작

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 
-const server = http.createServer((req, res) => {
-    
-    res.setHeader(
-        "Content-Type",
-        "application/json; charset=utf-8"
-    );
+let messages = [
+    {
+        name : '동건',
+        text : '하이 예링'
+    }
+];
 
-    const messagesArray = [
-        {
-            name : '동건',
-            message : '하이'
-        },
-        {
-            name : '예린',
-            message : '안뇽'
-        }
-    ];
+const server = http.createServer((req, res)=>{
 
-    res.end(
-        JSON.stringify(messagesArray)
-    );
-    // const indexPath = path.join(__dirname, 'index.html');
+    if(req.method === 'GET'){
 
-    // fs.readFile(indexPath, (err, data) => {
-    //     if(err){
-    //         res.statusCode = 500;
-    //         res.end('Error loading index.html');
-    //     } else {
-    //         res.setHeader('Content-Type', 'text/html; charset=utf-8');
-            
-    //         res.write(data);
-            
-    //         res.end();
-    //     }
-    // });
-    
+
+        res.setHeader(
+            "Content-Type",
+            "application/JSON; charset=utf-8"
+        );
+
+        res.end(
+            JSON.stringify(messages)
+        );
+
+    }
+
+    if(req.method === 'POST'){
+
+        let body = '';
+        req.on('data', (chunck)=>{
+            body += chunck;
+        })
+        req.on('end', ()=>{
+            messages.push(JSON.parse(body));
+
+            res.end("저장 완료");
+        })
+        
+
+    }
+
 
 });
 
-server.listen(3000, () => {
-    console.log('3000번 포트에서 서버 대기 중입니다.');
+server.listen(3000, ()=>{
+    console.log("서버가 실행중입니다");
 });
