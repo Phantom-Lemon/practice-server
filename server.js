fa357a41612c184d78493a99cf2619304ffe9389
Phantom-Lemon <donggun0524@naver.com>
2026-06-14 17:28:11 +0900

    html에서 요청하고 서버에서 보내기 (fetch)구현

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const server = http.createServer((request, response) => {
-    response.write('Hello World!');
-    response.end();
+const server = http.createServer((req, res) => {
+    
+    res.setHeader(
+        "Content-Type",
+        "application/json; charset=utf-8"
+    );
+
+    const data = {
+        name : '동건',
+        message : '하이'
+    };
+
+    res.end(
+        JSON.stringify(data)
+    );
+    // const indexPath = path.join(__dirname, 'index.html');
+
+    // fs.readFile(indexPath, (err, data) => {
+    //     if(err){
+    //         res.statusCode = 500;
+    //         res.end('Error loading index.html');
+    //     } else {
+    //         res.setHeader('Content-Type', 'text/html; charset=utf-8');
+            
+    //         res.write(data);
+            
+    //         res.end();
+    //     }
+    // });
+    
+
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('3000번 포트에서 서버 대기 중입니다.');
+});
